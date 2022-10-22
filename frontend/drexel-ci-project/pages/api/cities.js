39c8c13b6f6cea534270ed2ddaa98d607e4fca6c
Richard Li <48068570+clsscrch@users.noreply.github.com>
2022-10-21 20:23:32 -0400

    frontend stuff

---
+++ frontend/drexel-ci-project/pages/api/cities.js
@@ -0,0 +1,28 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { prisma } from '../../db';
+
+
+export default async function handler(req, res) {
+
+  const cities = await prisma.cities.findMany({
+    select: {
+      city: true,
+    },
+    orderBy: {
+      population: 'desc',
+    },
+    take: 25
+  });
+
+
+  //Change key to value for antd autocomplete
+  const citiesList = cities.map((city) => {
+    return {
+      value: city.city,
+      // label: city.city,
+    }
+  });
+
+  res.status(200).json(citiesList || []);
+
+}
