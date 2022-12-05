f0a7ac34ebddf18a6ee769873386d98612e58fe4
jessicaxjiang <jess.jiang22@gmail.com>
2022-12-05 01:32:21 -0500

    formatting and README

--- Apify.py
+++ Apify.py
@@ -8,37 +8,41 @@ client = ApifyClient("apify_api_R6A0sqF7uqr01hfDbAtwsMZaZou4D13Y3HbR")
 # jobArray = ["Software Engineer","Computer Science", "Data Science"]
 # cityArray = ["Philadelphia", "New York City", ]
 
-jobArray = ["Computer Science","Retail Cashier","Doctor","Restuarant Server","Receptionist","Event planner","Writer","Janitor","Construction Worker","Banker"]
-cityArray = ["New York, New York","Los Angeles, California","Chicago, Illinois","Houston, Texas","Phoenix, Arizona","Philadelphia, Pennsylvania","San Antonio, Texas","San Diego, California","Dallas, Texas","San Jose, California"]
+jobArray = ["Computer Science", "Retail Cashier", "Doctor", "Restuarant Server",
+            "Receptionist", "Event planner", "Writer", "Janitor", "Construction Worker", "Banker"]
+cityArray = ["New York, New York", "Los Angeles, California", "Chicago, Illinois", "Houston, Texas", "Phoenix, Arizona",
+             "Philadelphia, Pennsylvania", "San Antonio, Texas", "San Diego, California", "Dallas, Texas", "San Jose, California"]
 
 # Prepare the actor input
 for job in jobArray:
-        for city in cityArray:
-                now = datetime.now() # current date and time
-                date_time = now.strftime("%m-%d-%Y %H_%M")
-                filename = job + "-" + city + date_time
-                run_input = {
-                "position": job,
-                "country": "US",
-                "location": city,
-                "maxItems": 50,
-                "maxConcurrency": 5,
-                "extendOutputFunction": """($) => {
+    for city in cityArray:
+        now = datetime.now()  # current date and time
+        date_time = now.strftime("%m-%d-%Y %H_%M")
+        filename = job + "-" + city + date_time
+        run_input = {
+            "position": job,
+            "country": "US",
+            "location": city,
+            "maxItems": 50,
+            "maxConcurrency": 5,
+            "extendOutputFunction": """($) => {
                 const result = {};
 
                 """ + f"result.title = \"{filename}\";" + """
 
                 return result;
                 }""",
-                "proxyConfiguration": { "useApifyProxy": True },
-                }
-                # print(run_input)
-                # # Run the actor and wait for it to finish
-                run = client.actor("hynekhruska/indeed-scraper").call(run_input=run_input)
+            "proxyConfiguration": {"useApifyProxy": True},
+        }
+        # print(run_input)
+        # # Run the actor and wait for it to finish
+        run = client.actor(
+            "hynekhruska/indeed-scraper").call(run_input=run_input)
 
-                # Fetch and print actor results from the run's dataset (if there are any)
-                f = open(f"output/{filename}.txt", "wb")
-                # Download_Items should be deprecated but its still working? and its new replacement isnt working.
-                outputbytes = client.dataset(run["defaultDatasetId"]).download_items(item_format='json')
-                f.write(outputbytes)
-                f.close()
+        # Fetch and print actor results from the run's dataset (if there are any)
+        f = open(f"output/{filename}.txt", "wb")
+        # Download_Items should be deprecated but its still working? and its new replacement isnt working.
+        outputbytes = client.dataset(
+            run["defaultDatasetId"]).download_items(item_format='json')
+        f.write(outputbytes)
+        f.close()
---
+++ README.md
@@ -0,0 +1,3 @@
+# Apify
+Using Apify, we are able to use an API that scraps the Indeed jobs website. 
+The code in Apify.py current iterates through a list of jobs and a list of cities, calling the worker job with each combination separately. All of the results are written to a csv file with data that will need to be uploaded to our database.
