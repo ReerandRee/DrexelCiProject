fb5aceb3f1acef9abb8ca274f21c06549f3ee643
Richard Li <48068570+clsscrch@users.noreply.github.com>
2022-12-04 23:17:31 -0500

    Frontend

--- frontend/drexel-ci-project/components/SelectCity.js
+++ frontend/drexel-ci-project/components/SelectCity.js
@@ -2,21 +2,27 @@ import { Select } from "antd"
 import { useState } from 'react';
 import PageLayout from "./PageLayout";
 
-const SelectCity = ({ cities }) => {
+const SelectCity = (props) => {
   // const [userInput, setUserInput] = useState();
+  // const [selectedCities, setSelectedCities] = useState([]);
 
+  const handleChange = (value) => {
+    console.log(value);
+    props.setSelectedCities(value);
+  }
   return (
     <>
       <Select
-        mode="multiple"
+        // mode="multiple"
         allowClear
         placeholder="Please select"
         style={{ width: '50vw' }}
+        onChange={value => handleChange(value)}
       // value={userInput}
       >
-        {cities.map((city) => {
+        {props.cities.map((city) => {
           return (
-            <Select.Option key={city.city + "," + city.state_name}>
+            <Select.Option key={city.city + ", " + city.state_name}>
               {city.city}, {city.state_name}
             </Select.Option>
           )
