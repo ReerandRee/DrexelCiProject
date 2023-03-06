c8abc310a4716c349c5b4fa20be7ba61ba95d12e
ReerandRee <lelandsly@outlook.com>
2023-03-06 16:43:45 -0500

    Save file and better parsing

--- Apify.py
+++ Apify.py
@@ -23,33 +23,61 @@ try:
 except:
     print("Failed to connect to database. Please try again.")
 
-def apify_call(csvFile, citynumber = -1):
+def apify_call(titlescsv, locationscsv, maxReads):
     
     # Original Code for scheduling tasks
 
     client = ApifyClient(os.getenv("API_KEY_PROD"))
-    if citynumber == -1:
-        citynumber = datetime.now().date().day - 1
 
-    jobDF = pd.read_csv(csvFile)
-    jobDF.dropna(inplace=True)
+    #Get CSV data
+    titlesDF = pd.read_csv(titlescsv)
+    titlesDF.dropna(inplace=True)
+    citiesDF = pd.read_csv(locationscsv, delimiter=';')
+    citiesDF.dropna(inplace=True)
+    jobs = list(titlesDF['General Fields'])
+    cities = list(citiesDF['Cities'])
 
-    jobs = list(jobDF['General Fields'])
+    #Read Save File
+    saveFile = open("save.txt", "r")
+    titleIndex = saveFile.readline()
+    titleIndex = titleIndex.split(':')
 
-    cities = list(jobDF['Cities'])
+    try:
+            if len(titleIndex) == 2:
+                    titleIndex = int(titleIndex[1])
+            else:
+                titleIndex = 0
+    except:
+            print("Could not read Job Title Index from Save file, defualting to 0")
+            titleIndex = 0
+
+    cityIndex = saveFile.readline()        
+    cityIndex = cityIndex.split(':')
+    try:
+            if len(cityIndex) == 2:
+                    cityIndex = int(cityIndex[1])
+            else:
+                    cityIndex = 0
+    except:
+            print("Could not read City Index from Save file, defualting to 0")
+            cityIndex = 0
+    saveFile.close()
 
     #MANUAL OVERRIDE
     # jobs = ["Computer Science", "Data Science","Cyber Security","Software Engineering", "Web Developer"]
+    parsedJobs = 0
+    while (parsedJobs < maxReads):
+        job = jobs[titleIndex]
+        city = cities[cityIndex]
+        print(job, city)
 
-    for job in jobs:
-        print(job, cities[citynumber])
         now = datetime.now()  # current date and time
         date_time = now.strftime("%m-%d-%Y %H_%M")
-        filename = job + "-" + cities[citynumber] + date_time
+        filename = job + "-" + city + date_time
         run_input = {
             "position": job,
             "country": "US",
-            "location": cities[citynumber],
+            "location": city,
             "maxItems": 150,
             "maxConcurrency": 5,
             "extendOutputFunction": """($) => {
@@ -65,9 +93,9 @@ def apify_call(csvFile, citynumber = -1):
         # # Run the actor and wait for it to finish
         run = client.actor("hynekhruska/indeed-scraper").call(run_input=run_input)
 
-    # Fetch and print actor results from the run's dataset (if there are any)
-    # f = open(f"outputschedule/{filename}.txt", "wb")
-    # Download_Items should be deprecated but its still working? and its new replacement isnt working.
+        # Fetch and print actor results from the run's dataset (if there are any)
+        # f = open(f"outputschedule/{filename}.txt", "wb")
+        # Download_Items should be deprecated but its still working? and its new replacement isnt working.
         outputbytes = client.dataset(
             run["defaultDatasetId"]).download_items(item_format='json')
 
@@ -90,7 +118,7 @@ def apify_call(csvFile, citynumber = -1):
                                                     requirements, 
                                                     description,
                                                     indeedLink) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''', 
-                                (i['id'],  i['positionName'], i['company'], i['location'], job, cities[citynumber], i['scrapedAt'], postedat, i['salary'], None, None, i['description'], i['url']))
+                                (i['id'],  i['positionName'], i['company'], i['location'], job, city, i['scrapedAt'], postedat, i['salary'], None, None, i['description'], i['url']))
             except:
                 print("Error inserting job id: " + i['id'])
             conn.commit()
@@ -98,8 +126,23 @@ def apify_call(csvFile, citynumber = -1):
 
         print(str(len(jsonOutput)) + " jobs inserted.")
 
+        parsedJobs += len(jsonOutput)
+
+        #If we're not at the end of the file list, reset.
+        titleIndex += 1
+        if titleIndex >= len(jobs):
+             titleIndex = 0
+             cityIndex += 1
+             if cityIndex >= len(cities):
+                  cityIndex = 0
+
     cursor.close()
     conn.close()
+
+    saveFile = open("save.txt", "w")
+    saveStr = "Last Read Job Title Index : {}\nLast Read City Index : {}".format(titleIndex, cityIndex)
+    saveFile.write(saveStr)
+    saveFile.close()
 
     # f.write(outputbytes)
     # f.close()
@@ -118,16 +161,4 @@ def parse_posted_at(inputString):
     return None; 
 
 if __name__ == "__main__":
-
-    apify_call('job.csv')
-    # schedule.every().day.do(apify_call('job.csv'))
-    # while True:
-    #     schedule.run_pending()
-
-#     schedule.every(1).second.do(LeesCode, "job.csv")
-# # schedule.every().day.do(LeesCode, "job.csv")
-
-#     while (i != 30):
-#         timeit.timeit()
-
-        
+    apify_call('JobSearchTitles.csv', 'JobSearchLocations.csv', 10000)
